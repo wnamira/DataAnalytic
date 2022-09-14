a2e420b282b96ca319c932171c19907cfb763472
wnamira <113412980+wnamira@users.noreply.github.com>
2022-09-14 11:40:53 +0800

    Update mallcustomer-ml-app.py

--- mallcustomer-ml-app.py
+++ mallcustomer-ml-app.py
@@ -10,15 +10,6 @@ df.head()
 features = ['Annual_Income_(k$)', 'Spending_Score']
 X = df[features]
 
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=5)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
-
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
-
 # Perform the prediction by using the trained model
 step_size = 0.01
 
