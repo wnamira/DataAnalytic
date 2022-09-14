6f1c0b7f3fcf7e6fd0f68dbd70fc194fa6870bd8
wnamira <113412980+wnamira@users.noreply.github.com>
2022-09-14 16:47:00 +0800

    Update mallcustomer-app.py

--- mallcustomer-app.py
+++ mallcustomer-app.py
@@ -1,49 +1,40 @@
-file = "mall_customer.csv"
+#Import Packages
 
+import streamlit as st
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn import datasets
+import plotly.tools
+file = "mall_customer.csv"
 df = pd.read_csv(file)
 
-df.shape
-df.describe()
-df.head()
 
-features = ['Annual_Income_(k$)', 'Spending_Score']
-X = df[features]
+st.header("My Mall Customer App")
 
-import matplotlib.pyplot as plt 
-import numpy as np
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score']);
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['whole data', 'description','KMeans cluster'])
 
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=5)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
+if option=='whole data':
+    whole = df
+    st.table(whole)
+    
+if option=='description':
+    chart_data = df.describe()
+    st.table(chart_data)
 
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
-
-# Perform the prediction by using the trained model
-step_size = 0.01
-
-# Plot the Decision Boundaries
-x_min, x_max = min(X.iloc[:,0]) - 1, max(X.iloc[:,0]) + 1
-y_min, y_max = min(X.iloc[:,1]) - 1, max(X.iloc[:,1]) + 1
-x_values, y_values = np.meshgrid(np.arange(x_min,x_max,step_size), np.arange(y_min,y_max,step_size))
-
-# Predict labels for all points in the mesh
-predictions = kmeans.predict(np.c_[x_values.ravel(), y_values.ravel()])
-# Plot the results
-predictions = predictions.reshape(x_values.shape)
-plt.figure(figsize=(8,6))
-plt.imshow(predictions, interpolation='nearest', extent=(x_values.min(), x_values.max(), y_values.min(), y_values.max()), 
-           cmap=plt.cm.Spectral, aspect='auto', origin='lower')
-
-plt.scatter(X.iloc[:,0],X.iloc[:,1], marker='o', facecolors='grey',edgecolors='w',s=30)
-# Plot the centroids of the clusters
-centroids = kmeans.cluster_centers_
-plt.scatter(centroids[:,0], centroids[:,1], marker='o', s=200, linewidths=3, 
-           color='k', zorder=10, facecolors='black')
-
-plt.title('Centroids and boundaries calculated using KMeans Clustering', fontsize=16)
-plt.show()
+else:
+    #Preprocessing
+    features = ['Annual_Income_(k$)', 'Spending_Score']
+    X = df[features]
+    plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score']);
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(X)
+    y_kmeans = kmeans.predict(X)
+    plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
+    centers = kmeans.cluster_centers_
+    plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
+    st.pyplot()
+    
+    st.set_option('deprecation.showPyplotGlobalUse', False)
