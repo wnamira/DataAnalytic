bf67f29947a1ef2e552f26a678d6bf34d9d5266c
wnamira <113412980+wnamira@users.noreply.github.com>
2022-09-14 16:41:56 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -1,49 +1,40 @@
+#Import Packages
+
 import streamlit as st
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.cluster import KMeans
 from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+import plotly.tools
+file = "mall_customer.csv"
+df = pd.read_csv(file)
 
-st.write("""
-# Simple Iris Flower Prediction App
 
-This app predicts the **Iris flower** type!
-""")
+st.header("My Mall Customer App")
 
-st.sidebar.header('User Input Parameters')
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['whole data', 'description','KMeans cluster'])
 
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
+if option=='whole data':
+    whole = df
+    st.table(whole)
+    
+if option=='description':
+    chart_data = df.describe()
+    st.table(chart_data)
 
-df = user_input_features()
-
-st.subheader('User Input parameters')
-st.write(df)
-
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+else:
+    #Preprocessing
+    features = ['Annual_Income_(k$)', 'Spending_Score']
+    X = df[features]
+    plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score']);
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(X)
+    y_kmeans = kmeans.predict(X)
+    plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
+    centers = kmeans.cluster_centers_
+    plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
+    st.pyplot()
+    
+    st.set_option('deprecation.showPyplotGlobalUse', False)
